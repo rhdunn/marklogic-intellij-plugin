55541672228b93979a182cbe525ac1f5a1ce9f39
Reece Dunn <reece.dunn@67bricks.com>
2017-07-24 12:40:30 +0100

    Fix running on IntelliJ 2017.1.

--- src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/configuration/MarkLogicRunConfiguration.java
+++ src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/configuration/MarkLogicRunConfiguration.java
@@ -17,10 +17,7 @@ package uk.co.reecedunn.intellij.plugin.marklogic.configuration;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.Executor;
-import com.intellij.execution.configurations.ConfigurationFactory;
-import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.execution.configurations.RunConfigurationBase;
-import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.configurations.*;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
@@ -79,6 +76,10 @@ public class MarkLogicRunConfiguration extends RunConfigurationBase {
 
     MarkLogicRunConfiguration(@NotNull Project project, @NotNull ConfigurationFactory factory, String name) {
         super(project, factory, name);
+    }
+
+    @Override
+    public void checkConfiguration() throws RuntimeConfigurationException {
     }
 
     @NotNull
