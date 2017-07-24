8067c20932a3616e2f9ebe2016d48f2c4e271cfe
Reece H. Dunn <msclrhd@gmail.com>
2017-07-24 22:33:06 +0100

    Create an xdmp:eval options builder.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/query/options/EvalOptionsBuilder.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.marklogic.query.options;
+
+import org.jetbrains.annotations.NotNull;
+
+public class EvalOptionsBuilder implements OptionsBuilder {
+    private String contentDatabase = null;
+    private String modulesDatabase = null;
+    private String modulesRoot = null;
+
+    @Override
+    public void reset() {
+        contentDatabase = null;
+        modulesDatabase = null;
+        modulesRoot = null;
+    }
+
+    @Override
+    public void setContentDatabase(String contentDatabase) {
+        this.contentDatabase = contentDatabase;
+    }
+
+    @Override
+    public void setModulesDatabase(String modulesDatabase) {
+        this.modulesDatabase = modulesDatabase;
+    }
+
+    @Override
+    public void setModulesRoot(String modulesRoot) {
+        this.modulesRoot = modulesRoot;
+    }
+
+    @Override
+    public void build(@NotNull StringBuilder builder) {
+        builder.append("<options xmlns=\"xdmp:eval\">");
+        buildDatabaseOption(builder, "database", contentDatabase);
+        buildDatabaseOption(builder, "modules", modulesDatabase);
+        buildOption(builder, "root", modulesRoot);
+        builder.append("</options>");
+    }
+
+    private void buildDatabaseOption(StringBuilder options, String option, String database) {
+        if (database == null || database.isEmpty()) {
+            return;
+        }
+
+        final String selector = "{xdmp:database(\"" + database + "\")}";
+        buildOption(options, option, selector);
+    }
+
+    private void buildOption(StringBuilder options, String option, String value) {
+        if (value == null || value.isEmpty()) {
+            return;
+        }
+
+        options.append('<');
+        options.append(option);
+        options.append('>');
+        options.append(value);
+        options.append("</");
+        options.append(option);
+        options.append('>');
+    }
+}
---
+++ src/test/java/uk/co/reecedunn/intellij/plugin/marklogic/tests/query/options/EvalOptionsBuilderTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.marklogic.tests.query.options;
+
+import junit.framework.TestCase;
+import uk.co.reecedunn.intellij.plugin.marklogic.query.options.EvalOptionsBuilder;
+import uk.co.reecedunn.intellij.plugin.marklogic.query.options.OptionsBuilder;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SuppressWarnings("ConstantConditions")
+public class EvalOptionsBuilderTest extends TestCase {
+    public void testNoOptions() {
+        OptionsBuilder builder = new EvalOptionsBuilder();
+        StringBuilder vars = new StringBuilder();
+
+        builder.build(vars);
+
+        final String expected =
+            "<options xmlns=\"xdmp:eval\">" +
+            "</options>";
+        assertThat(vars.toString(), is(expected));
+    }
+
+    public void testContentDatabaseOption() {
+        OptionsBuilder builder = new EvalOptionsBuilder();
+        StringBuilder vars = new StringBuilder();
+
+        builder.setContentDatabase("lorem");
+        builder.build(vars);
+
+        final String expected =
+            "<options xmlns=\"xdmp:eval\">" +
+            "<database>{xdmp:database(\"lorem\")}</database>" +
+            "</options>";
+        assertThat(vars.toString(), is(expected));
+    }
+
+    public void testModulesDatabaseOption() {
+        OptionsBuilder builder = new EvalOptionsBuilder();
+        StringBuilder vars = new StringBuilder();
+
+        builder.setModulesDatabase("lorem");
+        builder.build(vars);
+
+        final String expected =
+            "<options xmlns=\"xdmp:eval\">" +
+            "<modules>{xdmp:database(\"lorem\")}</modules>" +
+            "</options>";
+        assertThat(vars.toString(), is(expected));
+    }
+
+    public void testModulesRootOption() {
+        OptionsBuilder builder = new EvalOptionsBuilder();
+        StringBuilder vars = new StringBuilder();
+
+        builder.setModulesRoot("lorem");
+        builder.build(vars);
+
+        final String expected =
+            "<options xmlns=\"xdmp:eval\">" +
+            "<root>lorem</root>" +
+            "</options>";
+        assertThat(vars.toString(), is(expected));
+    }
+
+    public void testResetOptions() {
+        OptionsBuilder builder = new EvalOptionsBuilder();
+        StringBuilder vars = new StringBuilder();
+
+        builder.setContentDatabase("lorem");
+        builder.setModulesDatabase("ipsum");
+        builder.setModulesRoot("/dolor");
+        builder.reset();
+
+        builder.build(vars);
+
+        final String expected =
+            "<options xmlns=\"xdmp:eval\">" +
+            "</options>";
+        assertThat(vars.toString(), is(expected));
+    }
+}
