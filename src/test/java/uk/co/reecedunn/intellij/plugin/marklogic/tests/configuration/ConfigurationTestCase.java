b2e8a1b2082b22a94cacfe4ab1b6e0235b404e3a
Reece H. Dunn <msclrhd@gmail.com>
2017-07-26 07:05:54 +0100

    Create a helper class for writing tests using MarkLogicRunConfiguration instances.

---
+++ src/test/java/uk/co/reecedunn/intellij/plugin/marklogic/tests/configuration/ConfigurationTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.marklogic.tests.configuration;
+
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.lang.Language;
+import com.intellij.mock.MockProjectEx;
+import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.LightVirtualFile;
+import com.intellij.testFramework.PlatformLiteFixture;
+import org.jetbrains.annotations.NonNls;
+import uk.co.reecedunn.intellij.plugin.marklogic.configuration.MarkLogicConfigurationFactory;
+import uk.co.reecedunn.intellij.plugin.marklogic.configuration.MarkLogicConfigurationType;
+import uk.co.reecedunn.intellij.plugin.marklogic.configuration.MarkLogicRunConfiguration;
+
+public abstract class ConfigurationTestCase extends PlatformLiteFixture {
+    private MarkLogicConfigurationFactory mFactory;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        Extensions.registerAreaClass("IDEA_PROJECT", null);
+        myProject = new MockProjectEx(getTestRootDisposable());
+
+        ConfigurationType configurationType = new MarkLogicConfigurationType();
+        mFactory = (MarkLogicConfigurationFactory)configurationType.getConfigurationFactories()[0];
+    }
+
+    public MarkLogicRunConfiguration createConfiguration() {
+        return (MarkLogicRunConfiguration)mFactory.createTemplateConfiguration(myProject);
+    }
+
+    public VirtualFile createVirtualFile(@NonNls String name, String text) {
+        VirtualFile file = new LightVirtualFile(name, Language.ANY, text);
+        file.setCharset(CharsetToolkit.UTF8_CHARSET);
+        return file;
+    }
+}
