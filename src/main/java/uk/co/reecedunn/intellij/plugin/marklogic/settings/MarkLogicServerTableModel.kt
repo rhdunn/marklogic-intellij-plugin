19b2a8eef50ef2e4c786ed6b3c29e757b48ea5ba
Reece H. Dunn <msclrhd@gmail.com>
2017-11-16 07:52:24 +0000

    Implement access to the MarkLogicServer data from the table model.

--- src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/settings/MarkLogicServerTableModel.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/settings/MarkLogicServerTableModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.co.reecedunn.intellij.plugin.marklogic.settings
 
 import uk.co.reecedunn.intellij.plugin.marklogic.resources.MarkLogicBundle
@@ -12,8 +27,10 @@ private val COLUMN_NAMES = arrayOf(
     MarkLogicBundle.message("marklogic.settings.server.password"))
 
 class MarkLogicServerTableModel : AbstractTableModel() {
+    var servers: Array<MarkLogicServer> = arrayOf()
+
     override fun getRowCount(): Int =
-        0
+        servers.size
 
     override fun getColumnCount(): Int =
         COLUMN_NAMES.size
@@ -21,6 +38,17 @@ class MarkLogicServerTableModel : AbstractTableModel() {
     override fun getColumnName(column: Int): String =
         COLUMN_NAMES[column]
 
-    override fun getValueAt(rowIndex: Int, columnIndex: Int): Any =
-        Any()
+    override fun getValueAt(rowIndex: Int, columnIndex: Int): Any {
+        if (rowIndex < 0 || rowIndex >= servers.size)
+            return Any()
+        return when (columnIndex) {
+            0 -> servers[rowIndex].displayName ?: ""
+            1 -> servers[rowIndex].hostname
+            2 -> servers[rowIndex].appServerPort
+            3 -> servers[rowIndex].adminPort
+            4 -> servers[rowIndex].username ?: ""
+            5 -> servers[rowIndex].password ?: ""
+            else -> Any()
+        }
+    }
 }
