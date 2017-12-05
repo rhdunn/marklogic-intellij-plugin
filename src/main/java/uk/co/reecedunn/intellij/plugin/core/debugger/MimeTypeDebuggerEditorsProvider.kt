4085080ebb6e206f173949803f561ac827ba11cf
Reece H. Dunn <msclrhd@gmail.com>
2017-12-05 07:45:28 +0000

    Factor out a MimeTypeDebuggerEditorsProvider helper class.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/core/debugger/MimeTypeDebuggerEditorsProvider.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.core.debugger
+
+import com.intellij.lang.Language
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import com.intellij.util.LocalTimeCounter
+import com.intellij.xdebugger.XSourcePosition
+import com.intellij.xdebugger.evaluation.EvaluationMode
+import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider
+
+open class MimeTypeDebuggerEditorsProvider(private var mimeType: String): XDebuggerEditorsProvider() {
+    fun setupFile(file: PsiFile) {
+    }
+
+    override fun createDocument(project: Project, text: String, sourcePosition: XSourcePosition?, mode: EvaluationMode): Document {
+        val type = fileType
+        val file = PsiFileFactory.getInstance(project).createFileFromText(
+            "debug." + type.getDefaultExtension(),
+            type,
+            text,
+            LocalTimeCounter.currentTime(),
+            true)
+        setupFile(file)
+        return PsiDocumentManager.getInstance(project).getDocument(file)!!
+    }
+
+    override fun getFileType(): FileType {
+        val lang = Language.findInstancesByMimeType(mimeType).first()
+        return lang.associatedFileType as FileType
+    }
+}
--- src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/debugger/stack/view/MarkLogicErrorProcessStarter.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/debugger/stack/view/MarkLogicErrorProcessStarter.kt
@@ -15,42 +15,16 @@
  */
 package uk.co.reecedunn.intellij.plugin.marklogic.debugger.stack.view
 
-import com.intellij.lang.Language
-import com.intellij.openapi.editor.Document
-import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiFileFactory
-import com.intellij.util.LocalTimeCounter
 import com.intellij.xdebugger.XDebugProcess
 import com.intellij.xdebugger.XDebugProcessStarter
 import com.intellij.xdebugger.XDebugSession
-import com.intellij.xdebugger.XSourcePosition
-import com.intellij.xdebugger.evaluation.EvaluationMode
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider
 import com.intellij.xdebugger.frame.XExecutionStack
-
-private class MarkLogicDebugEditorsProvider(private var mimeType: String): XDebuggerEditorsProvider() {
-    override fun createDocument(project: Project, text: String, sourcePosition: XSourcePosition?, mode: EvaluationMode): Document {
-        val type = fileType
-        val psiFile = PsiFileFactory.getInstance(project).createFileFromText(
-            "debug." + type.getDefaultExtension(),
-            type,
-            text,
-            LocalTimeCounter.currentTime(),
-            true)
-        return PsiDocumentManager.getInstance(project).getDocument(psiFile)!!
-    }
-
-    override fun getFileType(): FileType {
-        val lang = Language.findInstancesByMimeType(mimeType).first()
-        return lang.associatedFileType as FileType
-    }
-}
+import uk.co.reecedunn.intellij.plugin.core.debugger.MimeTypeDebuggerEditorsProvider
 
 private class MarkLogicErrorProcess(session: XDebugSession): XDebugProcess(session) {
     override fun getEditorsProvider(): XDebuggerEditorsProvider {
-        return MarkLogicDebugEditorsProvider("application/xquery")
+        return MimeTypeDebuggerEditorsProvider("application/xquery")
     }
 }
 
