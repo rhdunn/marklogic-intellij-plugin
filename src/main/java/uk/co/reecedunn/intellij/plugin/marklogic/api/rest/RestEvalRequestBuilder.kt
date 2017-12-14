7d3c46bb290ac4545886453311203077209a4e5a
Reece H. Dunn <msclrhd@gmail.com>
2017-12-14 21:28:26 +0000

    rest: support variables in eval requests

--- src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/api/rest/RestEvalRequestBuilder.kt
+++ src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/api/rest/RestEvalRequestBuilder.kt
@@ -15,17 +15,31 @@
  */
 package uk.co.reecedunn.intellij.plugin.marklogic.api.rest
 
+import com.google.gson.JsonObject
 import org.apache.http.client.methods.RequestBuilder
 import uk.co.reecedunn.intellij.plugin.marklogic.api.EvalRequestBuilder
 import uk.co.reecedunn.intellij.plugin.marklogic.api.Request
 
 class RestEvalRequestBuilder internal constructor(private val connection: RestConnection) : EvalRequestBuilder() {
     override fun build(): Request {
+        var variables: JsonObject? = null
+        for (name in variableNames) {
+            if (variables == null) variables = JsonObject()
+
+            val value = getVariable(name).content // itemType is not supported via the REST API
+            if (name.namespace != null) {
+                variables.addProperty("{${name.namespace}}${name.localname}", value)
+            } else {
+                variables.addProperty(name.localname, value)
+            }
+        }
+
         val builder = RequestBuilder.post("${connection.baseUri}/v1/eval")
         addParameter(builder, "xquery", XQuery)
         addParameter(builder, "javascript", javaScript)
         addParameter(builder, "database", contentDatabase)
         addParameter(builder, "txid", transactionID)
+        variables?.let { addParameter(builder, "vars", it.toString()) }
         return RestRequest(builder.build(), connection)
     }
 
