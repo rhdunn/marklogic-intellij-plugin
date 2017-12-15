52e53ad469ac532640654bababbaf79722ab3f44
Reece H. Dunn <msclrhd@gmail.com>
2017-12-15 22:13:50 +0000

    Convert MarkLogicLogViewFactory to Kotlin.

---
+++ src/main/java/uk/co/reecedunn/intellij/plugin/marklogic/ui/log/MarkLogicLogViewFactory.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2017 Reece H. Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.reecedunn.intellij.plugin.marklogic.ui.log
+
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+
+class MarkLogicLogViewFactory : ToolWindowFactory, DumbAware {
+    private var mLogView: MarkLogicLogViewUI? = null
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        if (mLogView == null) {
+            mLogView = MarkLogicLogViewUI(project)
+        }
+
+        val content = ContentFactory.SERVICE.getInstance().createContent(mLogView!!.panel, null, false)
+        val contentManager = toolWindow.contentManager
+        contentManager.removeAllContents(true)
+        contentManager.addContent(content)
+        contentManager.setSelectedContent(content)
+        toolWindow.show(null)
+    }
+}
